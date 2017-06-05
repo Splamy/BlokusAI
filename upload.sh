94fe37e18aa61b1263a722debb3bfb384bc54ba5
Splamy <splamyn@gmail.com>
2017-06-05 19:19:38 +0200

    improved upload script

--- upload.sh
+++ upload.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
-scp www/index.html www/style.css splamy:~/blokus/
-scp www/js/game.js splamy:~/blokus/js/
+base="www/"
+
+function upload() {
+    md5res="$(md5sum $base$1)"
+    if [ ! -f "$base$1.md5" ] || [ "$(cat $base$1.md5)" != "$md5res" ]; then
+        echo "File changed: $base$1"
+        echo "$md5res" > "$base$1.md5"
+        scp "$base$1" "splamy:~/blokus/$1"
+    fi
+}
+
+upload "index.html"
+upload "style.css"
+upload "js/game.js"
